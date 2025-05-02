0bcb6db4164fc163f8af7046e9d74c3211eea1d7
Andrey Hauryk <andrey.hauryk@gmail.com>
2025-05-02 14:38:32 +0300

    refactor from options api to composition

--- src/components/table/VueExcelColumn.vue
+++ src/components/table/VueExcelColumn.vue
@@ -2,173 +2,172 @@
   <div />
 </template>
 
-<script lang="ts">
-import { defineComponent, PropType } from "vue";
-import { ColumnProps } from "./types";
+<script setup lang="ts">
+import { onBeforeMount, getCurrentInstance } from "vue";
+import type { ColumnProps } from "./types";
 
-export default defineComponent({
-  props: {
-  // 🔧 ДАННЫЕ
-  field: { type: String, default: "dummy" },
-  label: { type: String, default: null },
-  type: { type: String as PropType<ColumnProps["type"]>, default: "string" },
-  width: { type: String, default: "100px" },
-  invisible: { type: Boolean, default: false },
-  readonly: { type: Boolean, default: null },
-  keyField: { type: Boolean, default: false },
-  sticky: { type: Boolean, default: false },
-  listByClick: { type: Boolean, default: null },
-  autoFillWidth: { type: Boolean, default: false },
-  hideDuplicate: { type: Boolean, default: false },
-  grouping: { type: String, default: null },
-  mandatory: { type: String, default: "" },
-  lengthLimit: { type: Number, default: 0 },
-  autocomplete: { type: Boolean, default: null },
-  pos: { type: Number, default: 0 },
-  summary: { type: String, default: null },
-  noSorting: { type: Boolean, default: null },
-
-  // 📦 СЛОЖНЫЕ СТРУКТУРЫ (можно рассматривать как данные)
-  options: {
-    type: [Array, Object, Function] as PropType<ColumnProps["options"]>,
-    default: null,
-  },
-  allowKeys: {
-    type: [Array, Function] as PropType<ColumnProps["allowKeys"]>,
-    default: null,
-  },
-  initStyle: {
-    type: [Object, Function] as PropType<ColumnProps["initStyle"]>,
-    default: null,
-  },
-
-  // ⚙️ ФУНКЦИИ
-  validate: { type: Function as PropType<ColumnProps["validate"]>, default: null },
-  link: { type: Function as PropType<ColumnProps["link"]>, default: null },
-  isLink: { type: Function as PropType<ColumnProps["isLink"]>, default: null },
-  valueFormatter: {
-    type: Function as PropType<ColumnProps["valueFormatter"]>,
-    default: null,
-  },
-  render: {
-    type: Function as PropType<ColumnProps["render"]>,
-    default: null,
-  },
-  toValue: {
-    type: Function as PropType<(text: string) => any>,
-    default(text: string): any {
-      if (["datetick"].includes(this.type)) {
-        return new Date(text + " GMT+0").getTime();
-      }
-
-      if (this.type === "map" && typeof this.options === "function") {
-        const list = this.options(text);
-        return Object.keys(list).find((k) => list[k] === text);
-      }
-      return text;
-    },
-  },
-  toText: {
-    type: Function as PropType<(val: any) => string>,
-    default(val: any, record: any): string {
-      if (this.keyField && val?.toString().startsWith("§")) return "";
-      if (val == null) return "";
-      const offset = new Date().getTimezoneOffset() * 60 * 1000;
-      let d: number;
-      switch (this.type) {
-        case "number":
-          return this.valueFormatter ? this.valueFormatter(val, record) : String(val);
-        case "boolean":
-          return val ? "Да" : "Нет";
-        case "date":
-          d = new Date(val).getTime();
-          if (!d) return "";
-          return new Date(d - offset).toISOString().slice(0, 10);
-        case "datetick":
-          d = new Date(val * 1 ? val * 1 : val).getTime();
-          if (!d) return "";
-          return new Date(d - offset).toISOString().slice(0, 10);
-        case "map":
-          return typeof this.options === "function"
-            ? this.options(val)?.[val]
-            : this.options?.[val] ?? "";
-        case "action":
-          return "";
-        case "list":
-          return val[0] ? val[0].slice(0, 19) : "";
-        default:
-          return String(val);
-      }
-    },
-  },
-  register: {
-    type: Function as PropType<ColumnProps["register"]>,
-    default: null,
-  },
-},
-  created() {
-    this.init();
-  },
-  methods: {
-    init() {
-      const defaultAllowKeys: Record<string, string[]> = {
-        number: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "-"],
-        date: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "-"],
-        datetick: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "-", " ", ":"],
-      };
-
-      if (
-        this.type === "map" &&
-        this.options.constructor.name === "AsyncFunction"
-      ) {
-        throw new Error("VueExcelColumn: Map does not support Async Function");
-      }
-      if (
-        !["string", "boolean", "password", "list", "action"].includes(
-          this.type
-        ) &&
-        !defaultAllowKeys[this.type]
-      ) {
-        throw new Error("VueExcelColumn: Not supported type: " + this.type);
-      }
-      const style = { ...this.initStyle };
-      this._autocomplete = this.autocomplete ?? this.type === "action";
-      this._readonly = this.readonly;
-      this._listByClick = this.type === "action";
-
-      this.$parent.registerColumn({
-        name: this.field,
-        label: this.label ?? this.field,
-        type: this.type,
-        width: this.width,
-        origWidth: this.width,
-        autoFillWidth: this.autoFillWidth,
-        validate: this.validate,
-        link: this.link,
-        isLink: this.isLink ?? (this.link ? () => true : null),
-        noSorting: this.noSorting !== null ?? this.$parent.noSorting,
-        keyField: this.keyField,
-        sticky: this.sticky,
-        allowKeys: this.allowKeys ?? defaultAllowKeys[this.type],
-        mandatory: this.mandatory,
-        lengthLimit: Number(this.lengthLimit),
-        autocomplete: this._autocomplete ?? this.$parent.autocomplete,
-        initStyle: style,
-        invisible: this.invisible,
-        readonly: this._readonly ?? this.$parent.readonly,
-        pos: Number(this.pos),
-        options: this.options,
-        summary: this.summary,
-        toValue: this.toValue,
-        toText: (...arg) => this.toText(...arg) || "",
-        register: this.register,
-        listByClick: this.listByClick ?? this._listByClick,
-        hideDuplicate: this.hideDuplicate ?? this.grouping,
-        grouping: this.grouping,
-        render: this.render,
-      });
-    },
-  },
+const props = withDefaults(defineProps<{
+  field: string;
+  label: string | null;
+  type: ColumnProps["type"];
+  width: string;
+  invisible: boolean;
+  readonly: boolean | null;
+  keyField: boolean;
+  sticky: boolean;
+  listByClick: boolean | null;
+  autoFillWidth: boolean;
+  hideDuplicate: boolean;
+  grouping: string | null;
+  mandatory: string;
+  lengthLimit: number;
+  autocomplete: boolean | null;
+  pos: number;
+  summary: string | null;
+  noSorting: boolean | null;
+  options: ColumnProps["options"];
+  allowKeys: ColumnProps["allowKeys"];
+  initStyle: ColumnProps["initStyle"];
+  validate: ColumnProps["validate"];
+  link: ColumnProps["link"];
+  isLink: ColumnProps["isLink"];
+  valueFormatter: ColumnProps["valueFormatter"];
+  render: ColumnProps["render"];
+  toValue: (text: string) => any;
+  toText: (val: any, record: any) => string;
+  register: ColumnProps["register"];
+}>(), {
+  field: "dummy",
+  label: null,
+  type: "string",
+  width: "100px",
+  invisible: false,
+  readonly: null,
+  keyField: false,
+  sticky: false,
+  listByClick: null,
+  autoFillWidth: false,
+  hideDuplicate: false,
+  grouping: null,
+  mandatory: "",
+  lengthLimit: 0,
+  autocomplete: null,
+  pos: 0,
+  summary: null,
+  noSorting: null,
+  options: null,
+  allowKeys: null,
+  initStyle: null,
+  validate: null,
+  link: null,
+  isLink: null,
+  valueFormatter: null,
+  render: null,
+  toValue: undefined,
+  toText: undefined,
+  register: null,
 });
-</script>
 
+const instance = getCurrentInstance();
+const parent = instance?.proxy?.$parent;
+
+const init = () => {
+  const defaultAllowKeys: Record<string, string[]> = {
+    number: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "-"],
+    date: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "-"],
+    datetick: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "-", " ", ":"],
+  };
+
+  if (props.type === "map" && props.options?.constructor?.name === "AsyncFunction") {
+    throw new Error("VueExcelColumn: Map does not support Async Function");
+  }
+
+  if (
+    !["string", "boolean", "password", "list", "action"].includes(props.type) &&
+    !defaultAllowKeys[props.type]
+  ) {
+    throw new Error("VueExcelColumn: Not supported type: " + props.type);
+  }
+
+  const style = { ...(typeof props.initStyle === 'function' ? props.initStyle() : props.initStyle) };
+  const _autocomplete = props.autocomplete ?? (props.type === "action");
+  const _readonly = props.readonly;
+  const _listByClick = props.type === "action";
+
+  const finalToValue = props.toValue ?? ((text: string) => {
+    if (["datetick"].includes(props.type)) {
+      return new Date(text + " GMT+0").getTime();
+    }
+    if (props.type === "map" && typeof props.options === "function") {
+      const list = props.options(text);
+      return Object.keys(list).find((k) => list[k] === text);
+    }
+    return text;
+  });
+
+  const finalToText = props.toText ?? ((val: any, record: any): string => {
+    if (props.keyField && val?.toString().startsWith("§")) return "";
+    if (val == null) return "";
+    const offset = new Date().getTimezoneOffset() * 60 * 1000;
+    let d: number;
+    switch (props.type) {
+      case "number":
+        return props.valueFormatter ? props.valueFormatter(val, record) : String(val);
+      case "boolean":
+        return val ? "Да" : "Нет";
+      case "date":
+        d = new Date(val).getTime();
+        if (!d) return "";
+        return new Date(d - offset).toISOString().slice(0, 10);
+      case "datetick":
+        d = new Date(val * 1 ? val * 1 : val).getTime();
+        if (!d) return "";
+        return new Date(d - offset).toISOString().slice(0, 10);
+      case "map":
+        return typeof props.options === "function"
+          ? props.options(val)?.[val]
+          : props.options?.[val] ?? "";
+      case "action":
+        return "";
+      case "list":
+        return val[0] ? val[0].slice(0, 19) : "";
+      default:
+        return String(val);
+    }
+  });
+
+  parent?.registerColumn?.({
+    name: props.field,
+    label: props.label ?? props.field,
+    type: props.type,
+    width: props.width,
+    origWidth: props.width,
+    autoFillWidth: props.autoFillWidth,
+    validate: props.validate,
+    link: props.link,
+    isLink: props.isLink ?? (props.link ? () => true : null),
+    noSorting: props.noSorting !== null ?? parent?.noSorting,
+    keyField: props.keyField,
+    sticky: props.sticky,
+    allowKeys: props.allowKeys ?? defaultAllowKeys[props.type],
+    mandatory: props.mandatory,
+    lengthLimit: Number(props.lengthLimit),
+    autocomplete: _autocomplete ?? parent?.autocomplete,
+    initStyle: style,
+    invisible: props.invisible,
+    readonly: _readonly ?? parent?.readonly,
+    pos: Number(props.pos),
+    options: props.options,
+    summary: props.summary,
+    toValue: finalToValue,
+    toText: (...args) => finalToText(...args) || "",
+    register: props.register,
+    listByClick: props.listByClick ?? _listByClick,
+    hideDuplicate: props.hideDuplicate ?? props.grouping,
+    grouping: props.grouping,
+    render: props.render,
+  });
+};
+
+onBeforeMount(init);
+</script>
