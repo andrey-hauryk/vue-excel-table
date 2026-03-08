228f819ae9320907aacce020e320e703f7afa5c7
Hauryk <hauryk.andrey@gmail.com>
2026-03-08 09:53:00 +0300

    update lib

--- preview/main.ts
+++ preview/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import VueExcelTable from "vue-excel-table";
+import "vue-excel-table/vue-excel-table.css";
 
 createApp(App).use(VueExcelTable).mount("#app");
